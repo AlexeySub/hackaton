c85493ceaa58a0df1689e9730645b30d1ace5524
AlexeySub <48951064+AlexeySub@users.noreply.github.com>
2019-11-09 11:23:18 +0300

    Update taskBlock.py

--- backend/blocks/taskBlock.py
+++ backend/blocks/taskBlock.py
@@ -19,9 +19,6 @@ def createTask(data):
 
 def getTasks(data):
     tasks = Tasks.objects.all()
-    for i in tasks:
-        if i.deadline < datetime.now():
-            Tasks.objects.filter(id=i['id']).update(status=True)
     return HttpResponse(renderers.JSONRenderer().render(tasks.values()))
 
 
